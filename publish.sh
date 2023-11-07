e816ba2dee13790ced3b55bfb3e84c516199880f
Nodeify <kw1k@nodeify.net>
2023-11-07 08:46:04 -0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Load the .env file
+# Load environment variables
 if [ -f .env ]; then
-    export $(cat .env | xargs)
+    while IFS= read -r line; do
+        # Skip empty lines and lines starting with #
+        if [[ -z "$line" || "$line" =~ ^# ]]; then
+            continue
+        fi
+        # Use eval to handle the quotes correctly
+        eval "export $line"
+    done < .env
 fi
 
-./contracts-dev/cli/cli.ts  contracts gns publishNewSubgraph \
+# Change to the contracts-dev directory
+cd "$HOME/migrate-hs/contracts-dev"
+
+# Execute the TypeScript CLI command
+./cli/cli.ts  contracts gns publishNewSubgraph \
   --mnemonic "$MNEMONIC" \
   --provider-url "$RPC_ENDPOINT" \
   --ipfs "https://ipfs.network.thegraph.com/" \
